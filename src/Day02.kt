7f68f1c05860a41556c76380b7c1a50733933efb
Donald Cowart <donco@amazon.com>
2021-12-02 15:16:23 +0000

    Part 2 solution complete, less regex more split, command and value and loop over input to increment horizontalPosition and depth.

--- src/Day02.kt
+++ src/Day02.kt
@@ -20,10 +20,38 @@ fun main() {
         return depth * forwardMovement
     }
 
+    fun part02(input: List<String>): Int {
+        // What do you get if you multiply your final horizontal position by your final depth?
+        // down X increases your aim by X units.
+        // up X decreases your aim by X units.
+        // forward X does two things:
+        //   It increases your horizontal position by X units.
+        //   It increases your depth by your aim multiplied by X.
+
+        var aim = 0
+        var depth = 0
+        var horizontalPosition = 0
+
+        for (command in input) {
+            val (k, v) = command.split(" ")
+            when (k) {
+                "up" -> aim -= v.toInt()
+                "down" -> aim += v.toInt()
+                "forward" -> {
+                    horizontalPosition += v.toInt()
+                    depth += aim * v.toInt()
+                }
+            }
+        }
+        return depth * horizontalPosition
+    }
+
     //test input from the problem text
     val testInput = readInput("Day02_test")
     check(part01(testInput) == 150)
+    check(part02(testInput) == 900)
 
     val day2Input = readInput("Day02")
     println(part01(day2Input))
+    println(part02(day2Input))
 }
