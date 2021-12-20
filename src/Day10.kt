c259fd913defe01765591196301986fd442f173a
Donald Cowart <donco@amazon.com>
2021-12-20 14:25:21 +0000

    Day 10 solution part 2, added method to calculate score while looping over input required for solution 1, these scores are appended to list. Modified part 1 solution to return both the corrupted list and the incomplete scores list, for solution 2, the list is sorted and the middle element is printed as the solution.

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,39 +1,65 @@
 fun main() {
 
-    fun part01(input: List<String>): MutableList<Char> {
-        val openingChars = mapOf(Pair('{', '}'), Pair('[', ']'), Pair('(', ')'), Pair('<', '>'))
-        val closingChars = mapOf(Pair('}', '{'), Pair(']', '['), Pair(')', '('), Pair('>', '<'))
+    val openingChars = mapOf(Pair('{', '}'), Pair('[', ']'), Pair('(', ')'), Pair('<', '>'))
+    val closingChars = mapOf(Pair('}', '{'), Pair(']', '['), Pair(')', '('), Pair('>', '<'))
 
+    fun scoreIncompleteQueue(inputQueue: MutableList<Char>): Long {
+        val scoring: Map<Char, Int> = mapOf(Pair(')', 1), Pair(']', 2), Pair('}', 3), Pair('>', 4))
+        var queueScore = 0L
+
+        inputQueue.reversed().forEach() { inputChar ->
+            queueScore = 5 * queueScore + (scoring[openingChars[inputChar]] ?: 0)
+        }
+
+        return queueScore
+    }
+
+    fun part01(input: List<String>): Pair<MutableList<Char>, MutableList<Long>> {
         val inputQueue = mutableListOf<Char>()
         val corruptQueue = mutableListOf<Char>()
+        val incompleteQueueScore = mutableListOf<Long>() //needed for part 2 solution
+        var isIncomplete: Boolean
 
         input.forEach lineLoop@{ inputLine ->
+            isIncomplete = false
+            inputQueue.clear()
             inputLine.forEach { inputChar ->
                 if (inputChar in openingChars.keys) {
                     inputQueue.add(inputChar)
                 } else {
                     if (closingChars[inputChar] == inputQueue.last()) {
                         inputQueue.removeAt(inputQueue.size - 1)
+                        isIncomplete = true
                     } else {
                         corruptQueue.add(inputChar)
+                        isIncomplete = false
                         return@lineLoop
                     }
-
                 }
             }
+            if (isIncomplete && inputQueue.isNotEmpty()) {
+                incompleteQueueScore.add(scoreIncompleteQueue(inputQueue))
+            }
         }
-        return corruptQueue
+        return Pair(corruptQueue, incompleteQueueScore)
     }
+
 
     val testInput = readInput("Day10_test")
     val scoring: Map<Char, Int> = mapOf(Pair(')', 3), Pair(']', 57), Pair('}', 1197), Pair('>', 25137))
 
-    var corruptChars = part01(testInput)
+    val (corruptChars, incompleteScores) = part01(testInput)
     println(corruptChars.sumOf { scoring[it] ?: 0 })
     check(corruptChars.sumOf { scoring[it] ?: 0 } == 26397)
+    println("Solution 2: " + incompleteScores.sorted()[incompleteScores.size / 2])
+    check(incompleteScores.sorted()[incompleteScores.size / 2] == 288957L)
 
     val input = readInput("Day10")
-    corruptChars = part01(input)
-    println(corruptChars.sumOf { scoring[it] ?: 0 })
+    val (corruptChars2, incompleteScores2) = part01(input)
+    println(corruptChars2.sumOf { scoring[it] ?: 0 })
+
+    // mixing in solution part 2 here...
+    println("Solution 2: " + incompleteScores2.sorted()[incompleteScores2.size / 2])
+
 
 }
